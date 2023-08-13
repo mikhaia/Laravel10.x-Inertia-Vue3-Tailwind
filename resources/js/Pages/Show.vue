09eb42be5b87169b8371b03e1b908b82ee9df31d
Alexandr Mikhailov <alexander@enclave.vn>
2023-08-13 17:11:48 +0700

    Add Stylus. Add Tailwind CSS

--- resources/js/Pages/Show.vue
+++ resources/js/Pages/Show.vue
@@ -2,13 +2,13 @@
 import Layout from './Layout.vue'
 import { Head } from '@inertiajs/vue3'
 
-defineProps({ user: Object })
+defineProps({ user: String })
 </script>
 
 <template>
   <Layout>
     <Head title="Welcome" />
     <h1>Welcome</h1>
-    <p>Hello {{ user.name }}, welcome to your first Inertia app!</p>
+    <p>Hello {{ user }}, welcome to your first Inertia app!</p>
   </Layout>
 </template>
